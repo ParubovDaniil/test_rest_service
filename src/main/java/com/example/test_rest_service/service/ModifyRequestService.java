6205911ed89ee81afe6c1f7453bac24e9a0db855
ParubovDaniil <ParubovDaniil@mail.ru>
2023-04-30 16:45:39 +0500

    Initial commit

---
+++ src/main/java/com/example/test_rest_service/service/ModifyRequestService.java
@@ -0,0 +1,6 @@
+package com.example.test_rest_service.service;
+
+import com.example.test_rest_service.model.Request;
+public interface ModifyRequestService {
+    void modifyRq(Request request);
+}
